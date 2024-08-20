840c435dfdfee2b183312fcbb07c6ceb15898e94
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-20 11:23:50 -0400

    Update main.js

--- src/content/tutorial/1-basics/1-introduction/2-deriving-data/_solution/main.js
+++ src/content/tutorial/1-basics/1-introduction/2-deriving-data/_solution/main.js
@@ -7,10 +7,10 @@ let doubledElement = document.querySelector("#doubled");
 
 element.addEventListener("click", () => count.set(count.get() + 1));
 
-effect(() => (element.innerHTML = `count is ${count.get()}`));
+effect(() => (element.innerText = `count is ${count.get()}`));
 
 effect(() => {
   let doubled = count.get() * 2;
 
-  doubledElement.innerHTML = `doubled is ${doubled}`;
+  doubledElement.innerText = `doubled is ${doubled}`;
 });
